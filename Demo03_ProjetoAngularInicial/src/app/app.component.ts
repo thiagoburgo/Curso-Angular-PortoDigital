7aa4a15391d6ae9c1d7d75296235c18eb15247f9
thiagoburgo <thiagoburgo@gmail.com>
2018-05-30 09:47:46 -0300

    Primeiro componente

--- Demo03_ProjetoAngularInicial/src/app/app.component.ts
+++ Demo03_ProjetoAngularInicial/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  count = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.title = `Hello ${this.count++}`;
+    }, 2000);
+  }
+
 }
---
+++ Demo03_ProjetoAngularInicial/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {
+    console.log('Contrutor do HomeComponent');
+  }
+
+  ngOnInit() {
+    console.log('OnInit do HomeComponent');
+  }
+
+}
